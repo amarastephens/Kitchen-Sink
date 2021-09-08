29a9858355e64be2336a25c8bc26c765ad421bc5
Amara Stephens <amarastephens@Amaras-MBP.attlocal.net>
2021-09-08 10:35:45 -0500

    first submission

---
+++ app.js
@@ -0,0 +1,54 @@
+let name = "Amara"
+const numberStates = 50
+
+let num1 = 4
+let num2 = 5
+// Add 4 and 5
+console.log(num1 += num2)
+
+function sayHello() {
+    alert("Hello World!")
+    sayHello()
+    sayHelloAlert('Hello World!')
+};
+
+function checkAge(age) {
+    if (age >= 21 && Name == Name);
+    else {
+        return deny("Sorry " + Name + ", you aren't old enough to view this page!");
+    }
+}
+
+let vegetables = ['Carrots', 'Corn', 'Celery', 'Cucumber', 'Tomato']
+console.log(vegetables)
+
+let pet = {
+    name: Emmy,
+    breed: Mix
+}
+console.log(pet)
+
+let friends = {
+    name1: "Jenna",
+    age1: 21,
+
+    name2: "Blake",
+    age2: 18,
+
+    name3: "TJ",
+    age3: 25,
+
+    name4: Ella,
+    age4: 12,
+
+    name5: "Ryan",
+    age5: 40
+}
+
+function getLength(string) {
+    let index = 0;
+    while (string.slice(index) !== '') {
+        index++;
+    }
+    return index;
+}
